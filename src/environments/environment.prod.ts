a6231a1438c062e3345e61b22d366a38b0e5667c
Morlé Koudeka <ndimorle@live.fr>
2022-12-22 20:07:27 +0000

    prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
-};
+    production: true,
+    baseUrl: '/admin-ui/',
+    schemaUrl: '/admin-ui/assets/config/schema.json', //asset path OR URL
+    logo: '/admin-ui/assets/images/logo.png', //asset path OR URL
+    keycloakConfig: {
+      url: 'https://sunbird-rc.sandbox.gouv.tg/auth',
+      realm: 'sunbird-rc',
+      clientId: 'registry-frontend'
+    }
+  };
